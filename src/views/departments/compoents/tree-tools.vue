a4cda9211130cd46c6261fa99b0564c81273648b
Hello-XY <1661230401@qq.com>
2022-08-05 11:47:05 +0800

    实现树形的静态组织架构

---
+++ src/views/departments/compoents/tree-tools.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-row type="flex" style="width: 100%">
+    <el-col>
+      <!-- 名称应该变成 对应的节点中的name -->
+      {{ treeNode.name }}
+    </el-col>
+    <el-col :span="4">
+      <el-row type="flex">
+        <!-- 两个内容 -->
+        <el-col> {{ treeNode.manager }} </el-col>
+        <el-col>
+          <!-- 下拉菜单 element -->
+          <el-dropdown>
+            <span> 操作<i class="el-icon-arrow-down" /> </span>
+            <!-- 下拉菜单 -->
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <template v-if="!isRoot">
+                <el-dropdown-item>编辑部门</el-dropdown-item>
+                <el-dropdown-item>删除部门</el-dropdown-item>
+              </template>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+// 该组件需要对外开放属性 外部需要提供一个对象 对象里需要有name  manager
+export default {
+  // props可以用数组来接收数据 也可以用对象来接收
+  // props: {   props属性: {  配置选项 }  }
+  props: {
+    treeNode: {
+      type: Object,
+      required: true
+    },
+    isRoot: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,20 +1,51 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <el-card class="box-card">
+        <!-- 头部 -->
+        <TreeTools :treeNode="company" :isRoot="true" />
+        <!-- 树形 -->
+        <el-tree :data="departs" :props="defaultProps" default-expand-all>
+          <template v-slot="{ data }">
+            <TreeTools :treeNode="data" />
+          </template>
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import TreeTools from './compoents/tree-tools.vue'
+import { getDeptsApi } from '@/api/departments'
 export default {
+  components: {
+    TreeTools
+  },
   data() {
-    return {}
+    return {
+      defaultProps: {
+        label: 'name'
+      },
+      departs: [
+        { name: '总裁办', children: [{ name: '董事会' }] },
+        { name: '行政部' },
+        { name: '人事部' }
+      ],
+      company: { name: '传智教育', manager: '负责人' }
+    }
   },
 
-  created() {},
+  created() {
+    this.getDeptsApi()
+  },
 
-  methods: {}
+  methods: {
+    async getDeptsApi() {
+      const res = await getDeptsApi()
+      this.departs = res.depts
+    }
+  }
 }
 </script>
 
